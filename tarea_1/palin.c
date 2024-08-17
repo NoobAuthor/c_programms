90282087aaa2fe4a8f89c830cfa3b460f236cb11
NoobAuthor <igor21.assis@outlook.com>
2024-08-17 11:32:26 -0400

    ?

--- tarea_1/palin.c
+++ tarea_1/palin.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
 #include "palin.h"
 
 int palindrome(uint32_t x) {
-    // Variables para almacenar los bits a comparar
-    uint32_t left, right;
+  // Variables para almacenar los bits a comparar
+  uint32_t left, right;
 
-    for (int i = 0; i < 4; i++) {
-        // Extraer el nibble más significativo
-        left = (x >> (28 - 4 * i)) & 0xF;
+  for (int i = 0; i < 4; i++) {
+    // Extraer el nibble más significativo
+    left = (x >> (28 - 4 * i)) & 0xF;
 
-        // Extraer el nibble menos significativo
-        right = (x >> (4 * i)) & 0xF;
-        
-        // Comparar los nibbles
-        if (left != right) {
-            return 0;  // No es palíndromo
-        }
+    // Extraer el nibble menos significativo
+    right = (x >> (4 * i)) & 0xF;
+
+    // Comparar los nibbles
+    if (left != right) {
+      return 0; // No es palíndromo
     }
-    return 1;  // Es palíndromo
+  }
+  return 1; // Es palíndromo
 }
